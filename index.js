4d687d59261beabf282dd4315c2de24720d9920c
Matt Linder <mjlinder218@gmail.com>
2021-05-31 12:20:57 -0700

    Added logic to write html to file

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
+// Required external modules
+const fs = require('fs');
 const inquirer = require('inquirer');
+// Required internal modules
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-// Initialize a new employee object
-// const employee = new Employee();
-
-// Start the process of adding team members
-// employee.buildTeamView();
-
-console.log("This is the **** app./nStart by entering information about the manager of your team.");
-
-
-//We start with the manager...
+// Start by displaying instructions to user
+console.log('Welcome to TeamView!\nStart by entering information about the manager of your team:');
+//The first entry will be the manager
 let employeeType = 'Add manager';
 
+//Function that writes to the HTML file
 function writeHtml(data) {
-  console.log('In writeHtml');
-  console.log('vvvvvvvvvvvvvvvvvvvvvvvvv');
-  console.log(data);
+  fs.appendFile('index.html', data, (err) => {if (err) console.error(err)});
 }
 
 //Function that prompts for info, saves it, and eventually calls the function that writes to the HTML file
 const askQuestions = () => {
-  console.log(employeeType + '   ssssssssssssss')   // **************
-
-  let employeeList = []
   let newEmployee = {}
-
+  let htmlString = '';
+  // Prompt for questions
   inquirer                // ******Add more validation
     .prompt([
       {
         type: 'input',
         message: "Name:",
         name: 'name',
+        validate(value) {
+          if (value.length){
+            return true;
+          } else {
+            console.log(" <- Entry invalid. Please enter name")
+            return false;
+          } 
+        } 
       },
       {
         type: 'input',
         message: 'Employee ID:',
         name: 'id',
+        validate(value) {
+          // if (Number.isInteger(value)){
+          if (!isNaN(value)) {
+            return true;
+          } else {
+            console.log(" <- Entry invalid. Please enter a numeric employee ID")
+            return false;
+          } 
+        }
       },
       {       
         type: 'input',
@@ -47,11 +57,10 @@ const askQuestions = () => {
         validate: function (email) {  // From https://gist.github.com/Amitabh-K/ae073eea3d5207efaddffde19b1618e8
           valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
           if (valid) {
-            console.log(" - Valid email address");
               return true;
           } else {
-              console.log(".  Please enter a valid email address")
-              return false;
+            console.log(" <- Entry invalid. Please enter a valid email address")
+            return false;
           }
         }
       },
@@ -60,62 +69,136 @@ const askQuestions = () => {
         message: 'Office number:',
         name: 'officenum',
         when: employeeType==='Add manager',
+        validate(value) {
+          if (!isNaN(value)) {
+            return true;
+          } else {
+            console.log(" <- Entry invalid. Please enter a numeric office number")
+            return false;
+          } 
+        }
       },
       {
         type: 'input',
-        message: 'github:',
-        name: 'officenum',
+        message: 'GitHub:',
+        name: 'github',
         when: employeeType==='Add engineer',
+        validate(value) {
+          if (value.length){
+            return true;
+          } else {
+            console.log(" <- Entry invalid. Please enter a GitHub account name")
+            return false;
+          } 
+        }
       },
       {
         type: 'input',
         message: 'School:',
         name: 'school',
         when: employeeType==='Add intern',
+        validate(value) {
+          if (value.length){
+            return true;
+          } else {
+            console.log(" <- Entry invalid. Please enter a school")
+            return false;
+          } 
+        }
       },
+      //Menu question - user selects another team member type or opts 
+      // to complete input
       {
         type: 'list',
-        message: 'What next?',    // *************
+        message: 'Add another team member?',    // *************
         name: 'menu',
-        choices: ['Add engineer', 'Add intern', 'Input complete'],  // *********
+        choices: ['Add engineer', 'Add intern', 'No - input complete'],  // *********
       },
     ])
     .then((data) => {
-      console.log('+++++++');
-      console.log(data);
-      // console.log('--------');
-      // console.log(data.menu);
       switch(employeeType) {
         case 'Add manager':
           newEmployee = new Manager(data.name, data.id, data.email, data.officenum);
           employeeType = data.menu;
-          // askQuestions();
+          // Add Bootstrap card with info/styling for manager
+          htmlString =` 
+            <div class="col-12 col-sm-6 col-lg-4 mb-3">
+              <div class="card">
+                <h3 class="card-header">${newEmployee.name}</h3>
+                <div class="card-body">
+                  <h4 class="card-subtitle mb-2 text-muted">${newEmployee.getRole()}</h4>
+                  <p class="card-text" style='line-height: 2;'>  
+                    <b>ID:</b> ${newEmployee.id}<br>
+                    <b>Email:</b> <a href="mailto:${newEmployee.email}">${newEmployee.email}</a><br>
+                    <b>Office number:</b> ${newEmployee.officenum}
+                  </p>
+                </div>
+              </div>
+            </div>
+            `;
           break;
         case 'Add engineer':
           newEmployee = new Engineer(data.name, data.id, data.email, data.github);
           employeeType = data.menu;
-          // askQuestions();
+          // Add Bootstrap card with info/styling for engineer
+          htmlString = 
+          `<div class="col-12 col-sm-6 col-lg-4 mb-3">
+            <div class="card">
+              <h3 class="card-header">${newEmployee.name}</h3>
+              <div class="card-body">
+                <h4 class="card-subtitle mb-2 text-muted">${newEmployee.getRole()}</h4>
+                <p class="card-text" style='line-height: 2;'>  
+                  <b>ID:</b> ${newEmployee.id}<br>
+                  <b>Email:</b> <a href="mailto:${newEmployee.email}">${newEmployee.email}</a><br>
+                  <b>GitHub:</b> <a href="https://www.github.com/${newEmployee.github}">${newEmployee.github}</a>
+                </p>
+              </div>
+            </div>
+          </div>
+          `;
           break;
         case 'Add intern':
           newEmployee = new Intern(data.name, data.id, data.email, data.school);
           employeeType = data.menu;
-          // askQuestions();
+          // Add Bootstrap card with info/styling for intern
+          htmlString = 
+          `<div class="col-12 col-sm-6 col-lg-4 mb-3">
+             <div class="card">
+             <h3 class="card-header">${newEmployee.name}</h3>
+             <div class="card-body">
+                 <h4 class="card-subtitle mb-2 text-muted">${newEmployee.getRole()}</h4>
+                 <p class="card-text" style='line-height: 2;'>  
+                  <b>ID:</b> ${newEmployee.id}<br>
+                  <b>Email:</b> <a href="mailto:${newEmployee.email}">${newEmployee.email}</a><br>
+                  <b>School:</b> ${newEmployee.school}
+                 </p>
+               </div>
+             </div>
+           </div>
+          `;
           break;
-        default:
-          console.log('HOW DID THIS HAPPEN!!!!'); // *******
-          employeeType = false;  // **********
+        // default:
+        //   console.error('HOW DID THIS HAPPEN!!!!'); // *******
       }
-      if (data.menu==='Input complete') {
-        writeHtml(newEmployee);
-        // If Finished, quit and call function to write info to file
+      if (data.menu==='No - input complete') {
+        htmlString += `
+      </section>
+      </div>
+    </section>
+    <hr />
+  </main>
+<script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
+</body>
+</html>
+        `;
+        writeHtml(htmlString);
+
       } else {
+        writeHtml(htmlString);
         askQuestions();
       }
     });
   
-  // employeeType = false;
 };
 
 askQuestions();
-
-
